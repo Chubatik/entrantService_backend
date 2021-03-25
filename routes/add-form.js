6f7e53c904dfe39759b85c3fecf6f9f5d3276e52
Chubatik <49166879+Chubatik@users.noreply.github.com>
2021-03-25 21:20:53 +0200

    Updated queries

--- routes/add-form.js
+++ routes/add-form.js
@@ -3,7 +3,8 @@ var router = express.Router();
 const connection = require('../database/connection');
 
 router.get('/', (req, res) =>{
-    let queryString = 'select * from privileges_for_entrant; select * from specialties'
+    let queryString = `select * from privileges_for_entrant order by privilege_name asc;
+                       select * from specialties order by specialty_name asc;`;
     connection.query(queryString)
         .then(result => {
             res.json({data : result[0]});
