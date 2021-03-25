7b30b3a9fc09b0a096eaade874f7d74e9892f96c
Chubatik <49166879+Chubatik@users.noreply.github.com>
2021-03-25 22:07:10 +0200

    Updated query

--- routes/view-list.js
+++ routes/view-list.js
@@ -5,7 +5,8 @@ const connection = require('../database/connection');
 router.get('/', (req, res) =>{
     let query = `select entrants.*, declarations.declaration_date from documents 
                 left join entrants on documents.entrant_id = entrants.entrant_id
-                left join declarations on documents.declaration_id = declarations.declaration_id`;
+                left join declarations on documents.declaration_id = declarations.declaration_id
+                order by entrants.entrant_surname asc;`;
     connection.query(query)
         .then(result => {
             res.json({data : result[0]});
